38729f33e070a49329db8a00f29620618d262c8e
Sung JinWoo <wlsdn42710801@gmail.com>
2025-06-06 13:11:36 +0900

    [기능추가] 카테고리 세분화 및 카테고리별 출력 기능 추가
    카테고리를 리스트로 만들어 세분화하고, 카테고리별로 결과를 출력하는 기능으 추가했습니다. 리스트로 만들어 수정이 쉽고, 코드도 리스트에 맞춰 움직입니다.
    카테고리값을 정수로 받아 구분하고 구분용 변수와 비교하여 같을시 출력하는 방식을 이용하였습니다.

--- OSS_Budget/budget.py
+++ OSS_Budget/budget.py
@@ -1,9 +1,11 @@
 import datetime
 from expense import Expense
 
+
 class Budget:
     def __init__(self):
         self.expenses = []
+        self.category = ["식비", "교통비", "주거통신"," 유흥", "기타"]
 
     def add_expense(self, category, description, amount):
         today = datetime.date.today().isoformat()
@@ -24,4 +26,20 @@ class Budget:
         total = sum(e.amount for e in self.expenses)
         print(f"총 지출: {total}원\n")
 
+
+    def list_category_expenses(self) :
+    #카테고리는 숫자와 사전형을 연결되있어 그 값을 이용하기 위한 변수
+       for output in range(1,5) :
+            print("="*20)
+            print(f"{self.category[output]} 항목")
+            for idx, expense in enumerate(self.expenses, 1):# expenses 리스트를 순회
+                if int(expense.category) == output:  # 특정 카테고리와 비교
+                    print(expense)
+            print()
+            
+
+            
+    def print_category(self) :
+        for i in range(0,len(self.category)):
+            print(f" {i+1} : {self.category[i]}", end = '  ')
 
--- OSS_Budget/main.py
+++ OSS_Budget/main.py
@@ -8,12 +8,18 @@ def main():
         print("==== 간단 가계부 ====")
         print("1. 지출 추가")
         print("2. 지출 목록 보기")
-        print("3. 총 지출 보기")
-        print("4. 종료")
+        print("3. 카테고리별 지출 보기")
+        print("4. 총 지출 보기")
+        print("5. 종료")
         choice = input("선택 > ")
 
         if choice == "1":
-            category = input("카테고리 (예: 식비, 교통 등): ")
+            budget.print_category()
+            category = input("\n카테고리를 고르세요>")
+            
+            if int(category) >len(budget.category):
+                print("잘못된 카테고리 선택입니다")
+                continue
             description = input("설명: ")
             try:
                 amount = int(input("금액(원): "))
@@ -26,9 +32,12 @@ def main():
             budget.list_expenses()
 
         elif choice == "3":
+            budget.list_category_expenses()
+            
+        elif choice == "4":
             budget.total_spent()
 
-        elif choice == "4":
+        elif choice == "5":
             print("가계부를 종료합니다.")
             break
 
